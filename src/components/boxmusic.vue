ed507148119c804b542ba927d9506d98bd055bed
maria <msmariaulfa33@gmail.com>
2019-02-22 14:59:23 +0700

    a

--- src/components/boxmusic.vue
+++ src/components/boxmusic.vue
@@ -1,9 +1,15 @@
 <template>
 <div>
+<<<<<<< HEAD
   <v-btn @click.prevent="playerStatus = true" color="info">Ready</v-btn>
   <v-btn @click.prevent="playerStatus = false" color="info">Cancel</v-btn>
     Poin: {{playerPoin}}
     Poin opponent: {{opponentPoin}}
+=======
+  <v-btn @click.prevent="startPlay" color="info">Start</v-btn>
+ Score: {{playerPoin}}
+ Score: {{opponentScore}}
+>>>>>>> a
     <!-- <button @click.prevent="musicPlay = false"> stop</button> -->
     <!-- <button @click.prevent="next"> next</button> -->
     {{startTime}}
@@ -33,6 +39,7 @@
 <script>
 
 import { db } from '@/api/firestore.js'
+import { setInterval } from 'timers'
 import axios from 'axios'
 export default {
   name: 'boxmusic',
@@ -122,10 +129,14 @@ export default {
             })
           }
           this.musicPlay = false
+<<<<<<< HEAD
           console.log('===')
           this.newMusics = []
           this.newTitles = []
           // db.collection('rooms').doc(this.$route.params.roomId)
+=======
+          db.collection('rooms').doc(this.$route.params.roomId)
+>>>>>>> a
         }
         this.counter = 5
         this.total--
@@ -223,10 +234,15 @@ export default {
         )
         .then(({ data: { data } }) => {
           let random = Math.floor(Math.random() * 2)
+<<<<<<< HEAD
           // console.log(this.titles, 'ini title')
           // console.log(this.music, 'ini music')
           this.newTitles.push(data[random].title)
           this.newMusics.push(data[random].preview)
+=======
+          this.titles.push(data[random].title)
+          this.musics.push(data[random].preview)
+>>>>>>> a
         })
         .catch(err => {
           console.log(err.response)
